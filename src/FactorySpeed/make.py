e276b94b0e6b55550ef21348a020e7e1a965dc3f
aco4 <anthonythegu@gmail.com>
2026-01-02 06:27:43 -0500

    Improve Factory Speed accuracy
    Use diffs (which multiply BEFORE addition of factory upgrades, not AFTER)

--- src/FactorySpeed/make.py
+++ src/FactorySpeed/make.py
@@ -1,23 +1,36 @@
 import os
+import json
 import zipfile
+import urllib.request
 
 base_dir = os.path.dirname(__file__)
-template_path = os.path.join(base_dir, 'nxFactorySpeed', 'multiplay', 'script', 'mods', 'init.js')
 
-# Read the template content
-with open(template_path, 'r') as f:
-    template = f.read()
+structure_url = "https://raw.githubusercontent.com/Warzone2100/warzone2100/master/data/mp/stats/structure.json"
 
-for i in [2, 3, 4, 5, 6, 7, 8, 9, 10]:
-    modifier = str(i)
-    replaced = template.replace('%%MODIFIER%%', modifier)
+with urllib.request.urlopen(structure_url) as response:
+    structure_json = json.loads(response.read().decode())
 
-    # Output zip path (no .zip extension)
+for modifier in [2, 3, 4, 5, 6, 7, 8, 9, 10]:
+
+    diff = {
+        "A0LightFactory": {
+            "productionPoints": structure_json["A0LightFactory"]["productionPoints"] * modifier,
+            "moduleProductionPoints": structure_json["A0LightFactory"]["moduleProductionPoints"] * modifier
+        },
+        "A0CyborgFactory": {
+            "productionPoints": structure_json["A0CyborgFactory"]["productionPoints"] * modifier,
+            "moduleProductionPoints": structure_json["A0CyborgFactory"]["moduleProductionPoints"] * modifier
+        },
+        "A0VTolFactory1": {
+            "productionPoints": structure_json["A0VTolFactory1"]["productionPoints"] * modifier,
+            "moduleProductionPoints": structure_json["A0VTolFactory1"]["moduleProductionPoints"] * modifier
+        }
+    }
+
     archive_name = f'{modifier}xFactorySpeed'
     archive_path = os.path.join(base_dir, archive_name)
 
-    # Create the zip archive (with no .zip extension)
     with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        zipf.writestr('multiplay/script/mods/init.js', replaced)
+        zipf.writestr(f'diffs/{archive_name}/stats/structure.json', json.dumps(diff, indent=2))
 
-    print(f'Created mod: {archive_path}')
+    print(archive_path)
